db8cbc7c3b9ce1adffc86304aafa5645bb38e4de
xavi-rp <xavi.rotllan.puig@gmail.com>
2018-05-25 10:36:17 +0200

    Including information of the script

--- gbif_data.r
+++ gbif_data.r
@@ -13,10 +13,30 @@
 #
 # Created by: Xavier Rotllan-Puig (xavi.rotllan.puig@gmail.com)
 #
-# Description: The aim of this script is 
+# Description: The aim of this script is to define the function GetBIF(), which
+#              is used to download species occurrences from GBIF (Global Biodiversity
+#              Information Facility ), and saves them as a csv data set.
+#              It is based on several functions included in the package "rgbif"
+#              (Chamberlain, 2017).
+#              GetBIF() retrieve your GBIF credentials (user and password) and 
+#              automatically checks in a loop until the request of data made to
+#              GBIF is ready and starts the download. Finally, it saves the data 
+#              in a csv file.
 # 
+# Inputs:
+#       - The location of a csv file with the names of the species to be downloaded
+#       - For security reasons, your GBIF credentials (user, password and email) 
+#         can be loaded from a RData file (location needs to be given). Otherwise, 
+#         they can be passed as arguments
+#       
 # 
-# 
+# Outputs:
+#       - A csv file with 3 columns (species, decimalLatitude, decimalLongitude)
+#         
+# References:
+#       - Scott Chamberlain (2017). rgbif: Interface to the Global 'Biodiversity' 
+#         Information Facility 'API'. R package version 0.9.8. 
+#         https://CRAN.R-project.org/package=rgbif
 
 
 # ------------------------------------------
@@ -81,7 +101,7 @@ data1 <- data.frame()
 
 for (sps in specs){
   cat(paste0("Reading data for ", sps), "\n")
-  load(paste0("download_info_", sps, ".RData"))
+  load(paste0("download_info_", sps, ".RData"), verbose = TRUE)
   
   # Reading in data
   #load("download_info.RData", verbose = TRUE)
